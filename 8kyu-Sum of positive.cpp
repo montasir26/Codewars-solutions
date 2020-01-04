095f18d5313dd940dfd17f9e0970d6e91a3171c8
Irfanul Montasir <mdirfanmusa@gmail.com>
2020-01-04 20:50:59 +0600

    Create 8kyu-Sum of positive.cpp

---
+++ 8kyu-Sum of positive.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+int positive_sum (const std::vector<int> arr){
+  int sum=0;
+  for(int i=0;i<arr.size();i++){
+     
+     if(arr[i]>0){
+      
+      sum=sum+arr[i];
+
+     }
+ }
+  
+  
+  return sum;
+}
