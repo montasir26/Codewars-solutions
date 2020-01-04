6e18e5fb9be3b785f83d7c5b3ccc929eb06b071c
Irfanul Montasir <mdirfanmusa@gmail.com>
2020-01-04 20:21:23 +0600

    Create 8kyu-Grasshopper - Make change.py

---
+++ 8kyu-Grasshopper - Make change.py
@@ -0,0 +1,10 @@
+class Solution {
+
+public:
+const static  float money=10;
+const static  float candy=1.42;
+ const static float chips=2.40;
+  
+ const static float soda=1;
+ const static float change=money-candy-chips-soda;
+};
