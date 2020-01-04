1caad1683e0e569212f8e600f203941326c6ea5d
Irfanul Montasir <mdirfanmusa@gmail.com>
2020-01-04 20:19:13 +0600

    Create 8kyu-Invert values.cpp

---
+++ 8kyu-Invert values.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+std::vector<int> invert(std::vector<int> values)
+{
+
+        for(int i=0;i<values.size();i++){
+            
+            values[i]=values[i]*(-1);
+        
+        }  
+    return {values};
+}
